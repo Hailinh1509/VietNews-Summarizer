2ad4fd185888f21b20c22271e4d63fc2260aae6f
Hailinh1509 <linhham1509@gmail.com>
2026-06-01 15:48:26 +0700

    Padding & Truncation

--- src/model.py
+++ src/model.py
@@ -7,3 +7,29 @@ def get_tokenizer(model_checkpoint: str = "vinai/bartpho-word"):
 def get_model(model_checkpoint: str = "vinai/bartpho-word"):
     """Tải mô hình Seq2Seq BARTpho."""
     return AutoModelForSeq2SeqLM.from_pretrained(model_checkpoint)
+
+def preprocess_tokenize_function(examples, tokenizer, max_input_length: int = 1024, max_target_length: int = 256):
+    """
+    Thực hiện tokenize (chuyển chữ sang số), đồng thời giới hạn độ dài (truncation) 
+    và thêm padding về độ dài tối đa cho cả đầu vào (content) và nhãn (summary).
+    """
+    # Tokenize phần nội dung bài viết
+    model_inputs = tokenizer(
+        examples["content"],
+        max_length=max_input_length,
+        padding="max_length",
+        truncation=True
+    )
+
+    # Tokenize phần tóm tắt
+    labels = tokenizer(
+        text_target=examples["summary"],
+        max_length=max_target_length,
+        padding="max_length",
+        truncation=True
+    )
+
+    # Gán nhãn cho mô hình huấn luyện
+    model_inputs["labels"] = labels["input_ids"]
+
+    return model_inputs
